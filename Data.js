8b107643069732cc3319903cdca8c981c9ddf1cb
amarbessedik <amar.bessedik@gmail.com>
2020-06-30 22:14:06 -0400

    Application context created

---
+++ Data.js
@@ -0,0 +1,15 @@
+export default {
+    users :[
+        {username: 'regular', accessLevel: 1, password: 'user'},
+        {username: 'admin', accessLevel: 0, password: 'admin'}
+    ],
+    initProducts: [
+        {
+            name: 'shoes',
+            stock: 10,
+            price: 399.99,
+            shortDesc: 'This is a SHORT description for this product',
+            description: 'This a LONG description for this product. Gracias, gracias'
+        },
+    ]
+}
